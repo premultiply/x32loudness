8fa8ad1c2241a3c6dda6987674c27eacf1695829
premultiply <ulrich.peters@posteo.de>
2018-01-25 15:52:37 +0100

    Generic USB audio device support + Touchscreen res

--- r128meter.sh
+++ r128meter.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-SDL_AUDIODRIVER="alsa"
-AUDIODEV="plughw:CARD=XUSB"
+#export DISPLAY=":0.0"
+export SDL_AUDIODRIVER="alsa"
+export AUDIODEV="plughw:1"
 
 while true; do
-	ffplay -fflags nobuffer -loglevel quiet -f lavfi -i "amovie='plughw\:CARD=XUSB':f=alsa,ebur128=video=1:size=xga:peak=none:meter=9:metadata=0[out0][out1]" -nostats -fs -framedrop
-done
+	clear
+	ffplay -fflags nobuffer -loglevel quiet -f lavfi -i \
+	"amovie='plughw\:1':f=alsa,ebur128=video=1:size=800x480:peak=none:meter=9:metadata=0[out0][out1]" \
+	-nostats -framedrop -fs;
+done;
